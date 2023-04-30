360662b4ccbc925a019ccaadc9b1819d4660419b
Sting3r-dev <71461641+Sting3r-dev@users.noreply.github.com>
2023-04-30 18:14:43 +0000

    final_commit

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-function change_color()
-            {
-                document.getElementById('file1').style.backgroundColor='#ffffff';
-            }
-            
+
+const file_list = ['file1','file2','file3','file4']
+function show(btn) {
+    
+    var i;
+    for (i = 0; i < file_list.length; i++) {
+        if (file_list[i] === btn.id) {
+            btn.style.backgroundColor = '#fff';
+        } else{
+            document.getElementsByClassName('file-list-item')[i].style.backgroundColor = '#ececec';
+        }
+        
+    }
+    
+}
+
+//fajna funkcja ale nie udało mi się jej wykorzystac :(
+function displaytxt(){
+    var file = document.getElementById("myFile").files[0];
+          
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        var textArea = document.getElementById("editor-text-area");
+        textArea.value = e.target.result;
+    };
+    reader.readAsText(file);
+}
+
